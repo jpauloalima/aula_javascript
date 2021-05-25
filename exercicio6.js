080b3796d27e49cbfeb52ea7b11d76d807a1a348
jpauloalima <jpauloalima@gmail.com>
2021-05-25 12:26:15 -0300

    aula de iteração de array

---
+++ exercicio6.js
@@ -0,0 +1,17 @@
+
+
+const prompt = require('prompt-sync')();
+
+
+(() => {
+
+    var carMark = ["fiat", "chevrolet", "hyundai", "toyota",];
+    var carModel = ["corola", "palio", "onix", "hb20", "creta", "uno", "hilux", "s10", "toro"];
+
+    var count = 0;
+    while (count < carModel.length) {
+        console.log(carModel[count]);
+        count++
+    }
+
+})();
