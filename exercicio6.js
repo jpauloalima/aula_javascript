5a82507d08b7f1cfea8242d11176e30e1a0a3bf0
jpauloalima <jpauloalima@gmail.com>
2021-05-25 14:07:09 -0300

    alteração, separacao de carMark com carModel

--- exercicio6.js
+++ exercicio6.js
@@ -9,9 +9,14 @@ const prompt = require('prompt-sync')();
     var carModel = ["corola", "palio", "onix", "hb20", "creta", "uno", "hilux", "s10", "toro"];
 
     var count = 0;
+    while (count < carMark.length) {
+        console.log(carMark[count]);
+        count++
+    }
+    console.log("------------------------------");
+    var count = 0;
     while (count < carModel.length) {
         console.log(carModel[count]);
         count++
     }
-
 })();
