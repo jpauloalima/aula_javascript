baf0eac53b184ef778c2fa6457a281c1b7ec6272
jpauloalima <jpauloalima@gmail.com>
2021-06-07 17:14:19 -0300

    swith case , falta terminar

--- exercicio15.js
+++ exercicio15.js
@@ -10,53 +10,93 @@ const prompt = require('prompt-sync')();
     console.log("5 - Para falar com atendente");
     console.log("6 - Para Sair");
 
-    var option = prompt("O que voce deseja? ");
 
-    switch (option) {
+    var stopCondition = true;
+    while (stopCondition) {
+        var option = prompt("O que voce deseja? ");
+        switch (option) {
+            case '1':
+                var optionSupport = prompt("Voce escolheu suporte, deseja continuar? S/N ");
+                switch (optionSupport.toUpperCase()) {
+                    case 'S':
+                        console.log("1 - Suporte Hardware");
+                        console.log("2 - Suporte Software");
+                        var optionSupportNext = prompt("Qual das opcoes deseja ");
+                        switch (optionSupportNext) {
+                            case '1':
+                                return console.log("estamos transferindo para o suporte Hardware")
+                            case '2':
+                                return console.log("estamos transferindo para o suporte Software");
+                            default:
+                                console.log("escolha a opcao correta");
+                        }
+                    case 'N':
+                        return console.log("escolha uma nova opção")
+                }
+                break;
+            case '2':
+                console.log("Você escolheu financeiro");
+                var optionFinance = prompt("Deseja continuar? ");
+                switch (optionFinance.toUpperCase()) {
+                    case "S":
+                        console.log("1 - Pagamento em atraso");
+                        console.log("2 - Pagamento adiantado");
+                        var optionFinanceNext = prompt("escolha uma opção");
+                        switch (optionFinanceNext) {
+                            case "1":
+                                var stopEmailConfirmation = true;
+                                while (stopEmailConfirmation) {
+                                    var emailPayment = prompt("Digite seu email para enviarmos o boleto ");
+                                    var emailConfirmation = prompt(`seu email é: ${emailPayment}, esta correto? S/N`);
+                                    switch (emailConfirmation.toUpperCase()) {
+                                        case "S":
+                                            return console.log("estamos enviando seu email");
+                                        //stopEmailConfirmation = false;
+                                        case "N":
+                                            console.log("Por favor, digite novamente");
+                                            stopEmailConfirmation = true;
+                                            break;
+                                        default:
+                                            console.log("voce digitou errado")
+                                    }break;
+                                }
+                        }
+                        stopCondition = false;
+                }break;
+            case '3':
+                console.log("Queremo ouvir suas sugestões");
+                console.log("Digite no campo abaixo:");
+                prompt("")
+                break
+            case '4':
+                var work = prompt("Gostaria de ser nosso colaborador? S/N");
+                switch (work.toUpperCase()) {
+                    case "S":
+                        console.log("######## Por favor, Digite seus dados nos campos abaixo ########");
+                        var name = prompt("Digite seu nome completo:");
+                        var address = prompt("Digite seu endereço:");
+                        var phone = prompt("Digite se telefone:");
+                        var emailWork = prompt("Digite seu email:");
+                        var office = prompt("Digite o cargo desejado:");
+                        var candidateData =
+                            [name,
+                                address,
+                                phone,
+                                emailWork,
+                                office];
+                        console.log(candidateData);
+                        stopCondition = false;
+                }
+                break;
+            case '5':
 
-        case '1':
-            var optionSupport = prompt("Voce escolheu suporte, deseja continuar? S/N ");
+            case '6':
 
-            switch (optionSupport.toUpperCase()) {
-                case 'S':
-                    console.log("1 - Suporte Hardware");
-                    console.log("2 - Suporte Software");
-                    var optionSupportNext = prompt("Qual das opcoes deseja");
-
-
-                    break;
-                case 'N':
-                    return console.log("Estamos transferindo para a area de hardware ");
-            }
-            break;
-        case '2':
-            return console.log("Estamos transferindo para a area de software ");
-        case '2':
-            var optionFinance = prompt("voce escolheu financeiro , deseja continuar? S/N ")
-            // if (optionFinance.toUpperCase == "S") {
-            // console.log("Qual das opçoes abaixo voce deseja?")
-            //  console.log("1 - Para pagamento em atraso");
-            //  console.log("2 - Para pagameno adiantado");
-            break;
+            default:
+                console.log("ops... voce digitou algo errado");
+        }
 
 
 
-        case '3':
-            console.log("Queremo ouvir suas sugestões")
-            console.log("Digite no campo abaixo:");
-            prompt("")
-            break
-        case '4':
-
-        case '5':
-
-        case '6':
-
-        default:
-            console.log("ops... voce digitou algo errado");
     }
-
-
-
-
 })();
